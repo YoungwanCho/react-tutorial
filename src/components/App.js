22fd4475e6803731280a210ae5489d196d42f402
developer119 <developer119korea@gmail.com>
2018-03-11 02:10:25 +0900

    하드코딩된 전화번호부

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
-import Content from './Content';
-import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            value: Math.round(Math.random() * 100)
-        };
-        this.updateValue = this.updateValue.bind(this);
-    }
-
-    updateValue(randomValue) {
-        this.setState({
-            value: randomValue
-        });
-    }
-    
-    render() {
-        return (
-            <div>
-                <Header title={this.props.headerTitle} />
-                <Content title={this.props.contentTitle}
-                    body={this.props.contentBody} />
-                <RandomNumber number={this.state.value}
-                    onUpdate={this.updateValue} />
-            </div>
+    render(){
+        return  (
+            <Contacts/>
         );
     }
 }
 
-App.defaultProps = {
-    headerTitle: 'Default header',
-    contentTitle: 'Default contentTitle',
-    contentBody: 'Default contentBody'
-};
-
+class Contacts extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Contacts</h1>
+                <ul>
+                    <li>Abet 010-0000-0001</li>
+                    <li>Betty 010-0000-0002</li>
+                    <li>Chalie 010-0000-0003</li>
+                    <li>David 010-0000-0003</li>
+                </ul>
+            </div>
+        );
+    }
+}
 export default App;
