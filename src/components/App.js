a32b1251e53373185362a6cb864743e9264584a2
developer119 <developer119korea@gmail.com>
2018-03-11 02:18:37 +0900

    Contacts 컴포넌트 렌더링 부분 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 
 class App extends React.Component {
-    render(){
-        return  (
-            <Contacts/>
+    render() {
+        return (
+            <Contacts />
         );
     }
 }
 
 class Contacts extends React.Component {
-    render(){
+    render() {
         return (
             <div>
                 <h1>Contacts</h1>
                 <ul>
-                    <li>Abet 010-0000-0001</li>
-                    <li>Betty 010-0000-0002</li>
-                    <li>Chalie 010-0000-0003</li>
-                    <li>David 010-0000-0003</li>
+                    <ContactInfo name="Abet" phone="010-0000-0001" />
+                    <ContactInfo name="Betty" phone="010-0000-0002" />
+                    <ContactInfo name="Chalie" phone="010-0000-0003" />
+                    <ContactInfo name="David" phone="010-0000-0003" />
                 </ul>
             </div>
         );
+    }
+}
+
+class ContactInfo extends React.Component {
+    render() {
+        return (
+            <li>{this.props.name} {this.props.phone}</li>
+        )
     }
 }
 export default App;
