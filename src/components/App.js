6c9d88c02b11edeb3d34b27aba5fe6e90ccb6cae
developer119 <developer119korea@gmail.com>
2018-03-11 01:00:11 +0900

    Props 사용해보기

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Header from './Header';
 import Content from './Content';
 
 class App extends React.Component {
-    render() {
-        return (
+    render(){
+        return  (
             <div>
-                <Header />
-                <Content />
+                <Header title={ this.props.headerTitle }/>
+                <Content title={ this.props.contentTitle }
+                          body={ this.props.contentBody }/>
             </div>
         );
     }
 }
+
+App.defaultProps = {
+    headerTitle: 'Default header',
+    contentTitle: 5, //'Default contentTitle',
+    contentBody: undefined //'Default contentBody'
+};
 
 export default App;
