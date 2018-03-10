e30d6002f364e5e9a1edcdde93b8824c8f9f4410
developer119 <developer119korea@gmail.com>
2018-03-11 02:28:28 +0900

    mapping을 사용하여 연락처를 업데이트하였다.

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,26 @@ class App extends React.Component {
 }
 
 class Contacts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contactData: [
+                { name: "Abet", phone: "010-0000-0001" },
+                { name: "Betty", phone: "010-0000-0002" },
+                { name: "Charlie", phone: "010-0000-0003" },
+                { name: "David", phone: "010-0000-0004" }
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 <h1>Contacts</h1>
                 <ul>
-                    <ContactInfo name="Abet" phone="010-0000-0001" />
-                    <ContactInfo name="Betty" phone="010-0000-0002" />
-                    <ContactInfo name="Chalie" phone="010-0000-0003" />
-                    <ContactInfo name="David" phone="010-0000-0003" />
+                    {this.state.contactData.map((contact, i) => {
+                        return (<ContactInfo name={contact.name} phone={contact.phone} key={i} />);
+                    })}
                 </ul>
             </div>
         );
@@ -31,4 +42,5 @@ class ContactInfo extends React.Component {
         )
     }
 }
+
 export default App;
